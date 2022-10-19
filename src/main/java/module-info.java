4fd99e2d474d397e925e3ffb1ecb34f599c82b2f
Amaury Rousseau <kinrar@kinrar.io>
2022-10-19 04:31:53 +0200

    Provide module-info for Java 9+

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module io.blindnet.jwt {
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires org.bouncycastle.provider;
+
+    exports io.blindnet.jwt;
+}
